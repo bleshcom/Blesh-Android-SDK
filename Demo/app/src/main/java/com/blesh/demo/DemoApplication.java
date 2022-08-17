4b173c8f53b5c52fc6d69535bb84552ebdf886ec
Arda Yalçıner <arda.yalciner@blesh.com>
2022-08-17 15:31:49 +0300

    Enable multi-dex in the demo application

--- Demo/app/src/main/java/com/blesh/demo/DemoApplication.java
+++ Demo/app/src/main/java/com/blesh/demo/DemoApplication.java
@@ -1,6 +1,9 @@
 package com.blesh.demo;
 
 import android.app.Application;
+import android.content.Context;
+
+import androidx.multidex.MultiDex;
 
 import com.blesh.sdk.BleshSdk;
 import com.blesh.sdk.core.models.SdkConfiguration;
@@ -17,4 +20,11 @@ public class DemoApplication  extends Application {
                         .testMode(false)
                         .build());
     }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
+
 }
